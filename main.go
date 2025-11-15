9c2098f8f3c3d00f3f460a4d85527f6d5463a794
Michal Bohuslávek <mbohuslavek@gmail.com>
2025-11-15 11:37:24 +0100

    Add main.go with initial scan/check loop

---
+++ main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"io/fs"
+	"log"
+	"os"
+	"path/filepath"
+)
+
+func main() {
+	log.SetPrefix("phpqc: ")
+	log.SetFlags(0)
+
+	parseFile("stub/DateTime.php")
+
+
+
+	toAnalyze := []string{
+	}
+
+	for _, name := range toAnalyze {
+		file := parsedFiles[name]
+		if file == nil {
+			log.Println(name, "is not parsed")
+			continue
+		}
+		Check(file)
+	}
+
+	// allParsed := slices.Sorted(maps.Keys(parsedFiles))
+	// for _, name := range allParsed {
+	// 		continue
+	// 	}
+	// 	file := parsedFiles[name]
+	// 	Check(file)
+	// }
+}
+
+var parsedFiles = make(map[string]*File)
+
+func parseFile(filename string) {
+	f, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	fi, err := f.Stat()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if fi.IsDir() {
+		parseDir(filename)
+		return
+	}
+
+	file, err := Parse(f, filename, false)
+	if se, ok := err.(*SyntaxError); ok {
+		log.Fatalf("%s:%d:%d: %v", filename, se.Line, se.Column, se.Err)
+	} else if err != nil {
+		log.Fatal(err)
+	}
+	file.Path = filename
+	parsedFiles[filename] = file
+}
+
+func parseDir(filename string) {
+	err := filepath.WalkDir(filename, func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			log.Fatal(err)
+		}
+		if d.IsDir() {
+			return nil
+		}
+		switch filepath.Ext(d.Name()) {
+		default:
+			return nil
+		case ".php":
+		}
+
+		parseFile(path)
+		return nil
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+}
