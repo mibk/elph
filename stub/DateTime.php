1e66d89e3efe2daeb38c187cdf3df32dc170630d
Michal Bohuslávek <mbohuslavek@gmail.com>
2026-01-18 12:41:10 +0100

    Add DateTime stubs and expand ignore patterns

--- stub/DateTime.php
+++ stub/DateTime.php
@@ -2,9 +2,19 @@
 
 class DateTime
 {
+	static function createFromFormat(string $format, string $datetime): DateTime|false;
+
 	function modify(string $modifier): static;
 
 	function diff(DateTimeInterface $targetObject, bool $absolute = false): DateInterval;
 
 	function format(string $format): string;
+
+	function getTimestamp(): int;
+
+	function setTimestamp(int $timestamp);
+
+	function setTime(int $hour, int $minute, int $second = 0, int $microsecond = 0): DateTime;
+
+	function getLastErrors(): array|false
 }
--- stub/DateTimeImmutable.php
+++ stub/DateTimeImmutable.php
@@ -2,7 +2,13 @@
 
 class DateTimeImmutable
 {
+	static function createFromFormat(string $format, string $datetime): DateTimeImmutable|false;
+
 	function modify(string $modifier): static;
 
+	function diff(DateTimeInterface $targetObject, bool $absolute = false): DateInterval;
+
 	function format(string $format): string;
+
+	function createFromInterface(DateTimeInterface $object): DateTimeImmutable;
 }
